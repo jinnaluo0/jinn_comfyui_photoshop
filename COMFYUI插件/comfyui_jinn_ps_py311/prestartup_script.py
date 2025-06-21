ab8f73f65c90b1823ee378832b1c3169ce8976f0
jinnaluo0 <jinnaluo@163.com>
2025-06-21 10:20:14 +0800

    Delete COMFYUI插件 directory

--- COMFYUI插件/comfyui_jinn_ps_py311/prestartup_script.py
+++
@@ -1,8 +0,0 @@
-
-import time
-from nodes import KSampler,KSamplerAdvanced
-@classmethod
-def IS_CHANGED(cls, **kwargs):
-    return time.time()
-KSampler.IS_CHANGED = IS_CHANGED
-KSamplerAdvanced.IS_CHANGED = IS_CHANGED
